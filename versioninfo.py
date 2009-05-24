273bcc25a892aa5c25bfc308ebd2354f7f843a45
vlcekp1@gmail.com <vlcekp1@gmail.com@38f907be-24db-11de-a101-5ba453ef2bb8>
2009-05-24 08:53:50 +0000

    Miny update

--- versioninfo.py
+++ versioninfo.py
@@ -4,7 +4,7 @@ class info():
 	longName=	"Python editor for the blind"
 	version="1.01 r6"
 	description=	u"Zdrojově otevřený python editor"
-	url=u"nemáme"	
+	url=u"máme"	
 	copyrightInfo=u"""Copy	right (C) 2008 Pavel Vlček a Lukáš Tyrychtr 
 This program is covered by the GNU General Public License (Version 2). You are free to share or change this software in any way you like as long as you distribute the licence along with the software, and make all source code available to anyone who wants it. This applies to both original and modified copies of the software, plus any software that uses code taken from this software.
 For further details, you can view the licence online at:
