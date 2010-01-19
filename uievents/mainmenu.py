80cedc6920f9821e21c06b2f852ae3c44ac92872
lukastyrychtr@gmail.com <lukastyrychtr@gmail.com@38f907be-24db-11de-a101-5ba453ef2bb8>
2010-01-19 17:24:00 +0000

    Name globals isn't good for global variables. Use appenv instead.

--- uievents/mainmenu.py
+++ uievents/mainmenu.py
@@ -1,4 +1,4 @@
-# -- coding: UTF-8 --
+﻿# -- coding: UTF-8 --
 import re
 import wx
 import utils
@@ -6,104 +6,104 @@ import versioninfo
 import uievents.searchdialog
 import  os
 info = versioninfo.info()
-import globals
+import appenv
 aboutInfo=u"koukavé označení {0.name}, skutečné jméno {0.longName}, verze {0.version}. Jako popis je uvedeno {0.description}, k čemuž dodáváme, že url {0.url}. Licenční a copirightové texty vemte prosím následující: {0.copyrightInfo}".format(info)
 wildcard = u"Python soubory (*.py)|*.py|python soubory bez konzolových oken (*.pyw)|*.pyw|Textové soubory (*.txt)|*.txt|všechny druhy souborů (*.*)|*.*"
 def speak_read_all(EVT):
-	utils.speak(globals.policko.Value)
+	utils.speak(appenv.policko.Value)
 def speak_read_to_end(evt):
-	utils.speak(globals.policko.GetRange(globals.policko.InsertionPoint, globals.policko.LastPosition))
+	utils.speak(appenv.policko.GetRange(appenv.policko.InsertionPoint, appenv.policko.LastPosition))
 def exit(evt):
 	utils.promptsave()
 	utils.speak(u"Nashledanou uživateli se jménem {0}. Doufám, že se ještě shledáme.".format(wx.GetUserName()))	
-	globals.app.ExitMainLoop()
+	appenv.app.ExitMainLoop()
 
 def file_open(evt):
 	utils.promptsave()
-	path = wx.FileSelector(u"Zvolte si soubor k otevření", wildcard=wildcard, flags=wx.OPEN + wx.FD_FILE_MUST_EXIST, parent=globals.frame)
+	path = wx.FileSelector(u"Zvolte si soubor k otevření", wildcard=wildcard, flags=wx.OPEN + wx.FD_FILE_MUST_EXIST, parent=appenv.frame)
 	if  path != "":
-		globals.policko.Clear()
-		globals.filename = path
+		appenv.policko.Clear()
+		appenv.filename = path
 		fd = open(path, "r")
-		globals.policko.Value = fd.read()
+		appenv.policko.Value = fd.read()
 		fd.close()
 		utils.update_window_title()
 def file_save_as(evt):
-	path = wx.FileSelector(u"Zvolte jméno, pod kterým bude soubor uložen", wildcard=wildcard, flags=wx.SAVE, parent=globals.frame).encode("windows-1250")
-	globals.policko.SaveFile(path)
-	globals.filename = path
+	path = wx.FileSelector(u"Zvolte jméno, pod kterým bude soubor uložen", wildcard=wildcard, flags=wx.SAVE, parent=appenv.frame).encode("windows-1250")
+	appenv.policko.SaveFile(path)
+	appenv.filename = path
 
 def file_save(evt):
-	if globals.filename == "":
+	if appenv.filename == "":
 		file_save_as(evt)
-	elif globals.policko.IsModified() and globals.filename != "":
-		globals.policko.SaveFile(globals.filename)            
+	elif appenv.policko.IsModified() and appenv.filename != "":
+		appenv.policko.SaveFile(appenv.filename)            
 def help_about(EVT):
 	wx.MessageBox(aboutInfo, u"O programu", wx.ICON_INFORMATION)
 def help_help(EVT):
 	wx.MessageBox(u"Pokud teď čekáte hromadu pokynů, jste na špatném místě. Ale kdyby jste přeci jen něco chtěli skusit najít, tak na http://mpyw.googlecode.com možná něco najdete.", "pokyny", wx.ICON_INFORMATION)                                
 def edit_copy(EVT):
-	globals.policko.Copy()
+	appenv.policko.Copy()
 def edit_cut(evt):
-	globals.policko.Cut()
+	appenv.policko.Cut()
 def edit_paste(evt):
-	globals.policko.Paste()
+	appenv.policko.Paste()
 def file_new(evt):
 	utils.promptsave()
-	globals.filename = ""
-	globals.policko.Clear()
+	appenv.filename = ""
+	appenv.policko.Clear()
 	utils.update_window_title()
 
 def edit_go_to_line(evt):
-	radek = wx.GetNumberFromUser(u"Vložte číslo řádku, na který chcete přejít.", u"číslo řádku", u"přejít na řádek", 1, parent=globals.frame, min=1, max=globals.policko.NumberOfLines)
-	pos = globals.policko.XYToPosition(0, radek - 1)
-	globals.policko.InsertionPoint = pos
+	radek = wx.GetNumberFromUser(u"Vložte číslo řádku, na který chcete přejít.", u"číslo řádku", u"přejít na řádek", 1, parent=appenv.frame, min=1, max=appenv.policko.NumberOfLines)
+	pos = appenv.policko.XYToPosition(0, radek - 1)
+	appenv.policko.InsertionPoint = pos
 
 def edit_search(evt):
-	globals.search_dialog = globals.file.LoadDialog(globals.frame, "searchdialog")
-	globals.search_dialog.FindWindowByName("find").Bind(wx.EVT_BUTTON, uievents.searchdialog.search_ok)
-	globals.search_dialog.FindWindowByName("cancel").Bind(wx.EVT_BUTTON, lambda evt: globals.search_dialog.Hide())
-	globals.search_dialog.ShowModal()
+	appenv.search_dialog = appenv.file.LoadDialog(appenv.frame, "searchdialog")
+	appenv.search_dialog.FindWindowByName("find").Bind(wx.EVT_BUTTON, uievents.searchdialog.search_ok)
+	appenv.search_dialog.FindWindowByName("cancel").Bind(wx.EVT_BUTTON, lambda evt: appenv.search_dialog.Hide())
+	appenv.search_dialog.ShowModal()
 
 def speak_indent(evt):
-	line = globals.policko.PositionToXY(globals.policko.InsertionPoint)[1]
-	text = globals.policko.GetLineText(line)
+	line = appenv.policko.PositionToXY(appenv.policko.InsertionPoint)[1]
+	text = appenv.policko.GetLineText(line)
 	utils.speak(u"Úroveň {0}".format(utils.get_indent_level(text)))
 
 def edit_increase_indent(evt):
 	expr = re.compile("^", re.U + re.M)
-	start = globals.policko.Selection[0]
-	end = globals.policko.Selection[1]
+	start = appenv.policko.Selection[0]
+	end = appenv.policko.Selection[1]
 	if start == end:
 		#Some selection, indent in it
-		lineno = globals.policko.PositionToXY(globals.policko.InsertionPoint)[1]
-		start = globals.policko.XYToPosition(0, lineno)
-		len = globals.policko.GetLineLength(lineno)
+		lineno = appenv.policko.PositionToXY(appenv.policko.InsertionPoint)[1]
+		start = appenv.policko.XYToPosition(0, lineno)
+		len = appenv.policko.GetLineLength(lineno)
 		end = start + len
-	text = globals.policko.GetRange(start, end)
-	text = expr.sub(globals.conf["indent"]["unit"], text)
-	globals.policko.Replace(start, end, text)
+	text = appenv.policko.GetRange(start, end)
+	text = expr.sub(appenv.conf["indent"]["unit"], text)
+	appenv.policko.Replace(start, end, text)
 	utils.speak(u"Úroveň {0}".format(utils.get_indent_level(text)))
 
 def edit_decrease_indent(evt):
-	expr = re.compile("^%s"%globals.conf["indent"]["unit"], re.U + re.M)
-	start = globals.policko.Selection[0]
-	end = globals.policko.Selection[1]
+	expr = re.compile("^%s"%appenv.conf["indent"]["unit"], re.U + re.M)
+	start = appenv.policko.Selection[0]
+	end = appenv.policko.Selection[1]
 	if start == end:
-		lineno = globals.policko.PositionToXY(globals.policko.InsertionPoint)[1]
-		start = globals.policko.XYToPosition(0, lineno)
-		len = globals.policko.GetLineLength(lineno)
+		lineno = appenv.policko.PositionToXY(appenv.policko.InsertionPoint)[1]
+		start = appenv.policko.XYToPosition(0, lineno)
+		len = appenv.policko.GetLineLength(lineno)
 		end = start + len
-	text = globals.policko.GetRange(start, end)
+	text = appenv.policko.GetRange(start, end)
 	text = expr.sub("", text)
-	globals.policko.Replace(start, end, text)
+	appenv.policko.Replace(start, end, text)
 	utils.speak(u"Úroveň {0}".format(utils.get_indent_level(text)))
 
 def edit_select_all(evt):
-	globals.policko.SelectAll()
+	appenv.policko.SelectAll()
 
 def edit_undo(evt):
-	globals.policko.Undo()
+	appenv.policko.Undo()
 
 def edit_redo(evt):
-	globals.policko.Redo()
+	appenv.policko.Redo()
--- window.py
+++ window.py
@@ -1,13 +1,13 @@
 ﻿# -- coding: UTF-8 --
 import wx
 from wx import xrc
-import globals
-globals.file = xrc.XmlResource("window.xrc")
+import appenv
+appenv.file = xrc.XmlResource("window.xrc")
 # Načtení hlavního okénečka
-globals.frame = globals.file.LoadFrame(None, "myframe")
+appenv.frame = appenv.file.LoadFrame(None, "myframe")
 # Vytvoření wx objektů(kvůli přístupu k vlastnostem, nebo kvůli navázání událostí)) 
-globals.policko = xrc.XRCCTRL(globals.frame, "policko")
-globals.stav = xrc.XRCCTRL(globals.frame, "stav")
-globals.stav.StatusText  =u"Řádek 1 sloupec 1"
+appenv.policko = xrc.XRCCTRL(appenv.frame, "policko")
+appenv.stav = xrc.XRCCTRL(appenv.frame, "stav")
+appenv.stav.StatusText  =u"Řádek 1 sloupec 1"
 # vytvoření menu
-globals.searchitem = globals.frame.MenuBar.FindItemById(xrc.XRCID("search"))
+appenv.searchitem = appenv.frame.MenuBar.FindItemById(xrc.XRCID("search"))
